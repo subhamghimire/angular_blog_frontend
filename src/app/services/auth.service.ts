8cc334767a47a1dc30f8d12b1b343d5107c8ed90
subhamghimire <subham.ghimire26@gmail.com>
2022-08-01 18:29:55 +0545

    response token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   public API_URL = 'http://localhost:3000/api';
+  private currentUserSubject: BehaviorSubject<any> = new BehaviorSubject<any>(
+    null
+  );
 
   constructor(public $http: HttpClient) {}
 
   signUp(data: any) {
     return this.$http.post(`${this.API_URL}` + '/auth/register', data);
   }
+
   login(data: any) {
-    return this.$http.post(`${this.API_URL}` + '/auth/login', data);
+    return this.$http.post(`${this.API_URL}` + '/auth/login', data).pipe(
+      map((response: any) => {
+        if (response.access_token) {
+          localStorage.setItem(
+            'access_token',
+            JSON.stringify(response.access_token)
+          );
+          this.currentUserSubject.next(response.access_token);
+        }
+        return response.access_token;
+      })
+    );
   }
 }
