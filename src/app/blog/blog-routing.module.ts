150e991f0ed1d8a99a72e6f83c36336d566363fb
subhamghimire <subham.ghimire26@gmail.com>
2022-08-01 09:54:08 +0545

    remove unnecessary route

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogComponent } from './blog.component';
-import { AddComponent } from './components/add/add.component';
 
-const routes: Routes = [
-  { path: 'blogs', component: BlogComponent },
-  { path: 'blogs/add', component: AddComponent },
-];
+const routes: Routes = [{ path: 'blogs', component: BlogComponent }];
 
 @NgModule({
   declarations: [],
